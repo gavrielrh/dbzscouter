ccefe7768c07577a7b8f638df8815be9f19d6e81
Drew H <bindernews@gmail.com>
2018-02-18 17:49:34 -0500

    Make font work correctly.

--- app/src/main/java/se/rit/edu/dbzscouter/MainActivity.kt
+++ app/src/main/java/se/rit/edu/dbzscouter/MainActivity.kt
@@ -1,12 +1,19 @@
 package se.rit.edu.dbzscouter
 
+import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // You can get rid of this, but it does prove that our font works!
+        val tv = findViewById<TextView>(R.id.hello_text)
+        val face = Typeface.createFromAsset(assets, "fonts/Square.ttf")
+        tv.typeface = face
     }
 }
