999bdc82e67369f540a0473e44cc9251f5907500
KevKode <iamkevinarnett@gmail.com>
2018-02-19 17:34:19 -0500

    Compressed overlay files, scaled and centered overlay based on screen size

--- app/src/main/java/se/rit/edu/dbzscouter/UIView.kt
+++ app/src/main/java/se/rit/edu/dbzscouter/UIView.kt
@@ -4,11 +4,26 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import java.io.ByteArrayOutputStream
+
 
 class UIView(context: Context) : android.support.v7.widget.AppCompatImageView(context) {
-    private val bmp: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.scouteroverlay)
+    private var scouter_overlay: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.scouteroverlay)
+    private var top_position = 0f
+
+    override fun onCreateDrawableState(extraSpace: Int): IntArray {
+        val scaledWidth = context.resources.displayMetrics.widthPixels
+        val scaledHeight = (scouter_overlay.height * scaledWidth) / scouter_overlay.width
+
+        top_position = (context.resources.displayMetrics.heightPixels / 2f) - (scaledHeight / 2f)
+
+        scouter_overlay = Bitmap.createScaledBitmap(scouter_overlay, scaledWidth, scaledHeight, false)
+        scouter_overlay.compress(Bitmap.CompressFormat.PNG, 100, ByteArrayOutputStream())
+
+        return super.onCreateDrawableState(extraSpace)
+    }
 
     override fun onDraw(canvas: Canvas) {
-        canvas.drawBitmap(bmp, 0f, 0f, null)
+        canvas.drawBitmap(scouter_overlay, 0f, top_position, null)
     }
 }
