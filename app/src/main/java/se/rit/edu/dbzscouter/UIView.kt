1b430dd13f2fe4d3b967963e724ef425ddbacfa2
Drew H <bindernews@gmail.com>
2018-02-19 21:51:58 -0500

    This is not Python, we don't use underscores.

--- app/src/main/java/se/rit/edu/dbzscouter/UIView.kt
+++ app/src/main/java/se/rit/edu/dbzscouter/UIView.kt
@@ -8,22 +8,22 @@ import java.io.ByteArrayOutputStream
 
 
 class UIView(context: Context) : android.support.v7.widget.AppCompatImageView(context) {
-    private var scouter_overlay: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.scouteroverlay)
-    private var top_position = 0f
+    private var scouterOverlay: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.scouteroverlay)
+    private var topPosition = 0f
 
     override fun onCreateDrawableState(extraSpace: Int): IntArray {
         val scaledWidth = context.resources.displayMetrics.widthPixels
-        val scaledHeight = (scouter_overlay.height * scaledWidth) / scouter_overlay.width
+        val scaledHeight = (scouterOverlay.height * scaledWidth) / scouterOverlay.width
 
-        top_position = (context.resources.displayMetrics.heightPixels / 2f) - (scaledHeight / 2f)
+        topPosition = (context.resources.displayMetrics.heightPixels / 2f) - (scaledHeight / 2f)
 
-        scouter_overlay = Bitmap.createScaledBitmap(scouter_overlay, scaledWidth, scaledHeight, false)
-        scouter_overlay.compress(Bitmap.CompressFormat.PNG, 100, ByteArrayOutputStream())
+        scouterOverlay = Bitmap.createScaledBitmap(scouterOverlay, scaledWidth, scaledHeight, false)
+        scouterOverlay.compress(Bitmap.CompressFormat.PNG, 100, ByteArrayOutputStream())
 
         return super.onCreateDrawableState(extraSpace)
     }
 
     override fun onDraw(canvas: Canvas) {
-        canvas.drawBitmap(scouter_overlay, 0f, top_position, null)
+        canvas.drawBitmap(scouterOverlay, 0f, topPosition, null)
     }
 }
