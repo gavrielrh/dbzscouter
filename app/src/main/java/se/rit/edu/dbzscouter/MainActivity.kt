60e7bd2ca1b25b2fdb9795c3af8a55dd6e9b63a0
Drew H <bindernews@gmail.com>
2018-02-26 12:01:34 -0500

    Ask for permission instead of forgiveness.

--- app/src/main/java/se/rit/edu/dbzscouter/MainActivity.kt
+++ app/src/main/java/se/rit/edu/dbzscouter/MainActivity.kt
@@ -2,14 +2,24 @@
 
 package se.rit.edu.dbzscouter
 
+import android.Manifest
 import android.content.res.Configuration
 import android.hardware.Camera
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.FrameLayout
 import android.widget.ImageView
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.RECORD_AUDIO
+import android.support.v4.content.PermissionChecker.PERMISSION_GRANTED
+
+
+const val CAMERA_CODE = 0x01
+const val RECORD_AUDIO_CODE = 0x02
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var camera: Camera
     internal lateinit var layout: FrameLayout
     private lateinit var ui: ImageView
@@ -21,6 +31,31 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        if (ActivityCompat.checkSelfPermission(this, CAMERA) != PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(CAMERA), CAMERA_CODE)
+        } else {
+            loadCamera()
+        }
+        if (ActivityCompat.checkSelfPermission(this, RECORD_AUDIO) != PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(RECORD_AUDIO), RECORD_AUDIO_CODE)
+        } else {
+            loadMicrophone()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            CAMERA_CODE -> {
+                loadCamera()
+            }
+            RECORD_AUDIO_CODE -> {
+                loadMicrophone()
+            }
+        }
+    }
+
+    private fun loadCamera() {
         camera = turnOnCamera()
         cameraView = CameraView(this, camera)
 
@@ -28,14 +63,16 @@ class MainActivity : AppCompatActivity() {
         layout.addView(cameraView, 0)
 
         ui = findViewById(R.id.ui_view)
-
         ui = UIView(this)
-
-        micRecorder = MicRecorder()
-        turnOnMic()
 
         layout.addView(ui)
     }
+
+    private fun loadMicrophone() {
+        micRecorder = MicRecorder()
+        turnOnMic()
+    }
+
 
     /**
      * Opens up the camera and sets its orientation based on the phone's orientation
