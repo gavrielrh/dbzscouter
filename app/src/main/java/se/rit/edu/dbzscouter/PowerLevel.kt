71df2f4f05e200ebc951d520b5373633da66ef25
Curtis Shea <cgs8601@rit.edu>
2018-03-24 14:04:41 -0400

    Did more commenting of PowerLevel

--- app/src/main/java/se/rit/edu/dbzscouter/PowerLevel.kt
+++ app/src/main/java/se/rit/edu/dbzscouter/PowerLevel.kt
@@ -17,7 +17,7 @@ class PowerLevel(var micRecorder: IMicRecorder) {
         var powerLevel = 0.0
         var powerLevelPercentage = 0.0
 
-        // fluctuates between 0 and DECIBEL_WEIGHT percent of BASE_POWER_LEVEL, based on
+        // fluctuates between 0 and DECIBEL_WEIGHT of 1, based on
         // the decibels above min of MicRecorder
          var decibelPower = (((micRecorder.maxReasonableDecibels - micRecorder.decibelsAboveMin) / micRecorder.maxReasonableDecibels))
 
@@ -28,13 +28,16 @@ class PowerLevel(var micRecorder: IMicRecorder) {
             decibelPower = 0.0
         }
 
+        //
         powerLevelPercentage += decibelPower * DECIBEL_WEIGHT
 
-        // fluctuates between 0 and EMOTION_WEIGHT percent of BASE_POWER_LEVEL, based on
+        // fluctuates between 0 and EMOTION_WEIGHT percent of 1, based on
         // the emotion val of FaceGraphic
         powerLevelPercentage += (faceGraphic!!.getEmotionVal() * EMOTION_WEIGHT)
 
-        powerLevel = BASE_POWER_LEVEL * Math.pow(powerLevelPercentage, POWER_EXP_CONST)
+
+        // make the powerlevel an exponential curve, from 0 to 1.
+        powerLevel = MAX_POWER_LEVEL * Math.pow(powerLevelPercentage, POWER_EXP_CONST)
 
         return powerLevel.toInt()
     }
@@ -43,15 +46,29 @@ class PowerLevel(var micRecorder: IMicRecorder) {
         this.faceGraphic = faceGraphic
     }
 
-    fun getBasePowerLevel() : Int{
-        return BASE_POWER_LEVEL
+    fun getMaxPowerLevel() : Int{
+        return MAX_POWER_LEVEL
     }
 
     companion object {
-        private val BASE_POWER_LEVEL = 50000
+        // the max power level.
+        private val MAX_POWER_LEVEL = 50000
+
+        // Used to determine the power level curve.
+        // Make POWER_EXP_CONST a higher number for a steeper curve,
+        // and a lower number for a more linear curve.
+        // Must be at least 1, which would be a straight line.
+        // Between the range of 2.0 and 5.0 is recommended.
         private val POWER_EXP_CONST = 2.0
+
+        // between 0 and 1, representing the min value of
+        // decibel power level pre-weighting that we will actually add to powerLevelPercentage
+        private val MIN_DECIBEL_POWER_THRESHOLD = 0.3
+
+
+        // constant weights that affect their respective contributions to MAX_POWER_LEVEL
+        // each one must be a double between 0 and 1, and they must all sum to 1.
         private val EMOTION_WEIGHT = 0.9
         private val DECIBEL_WEIGHT = 0.1
-        private val MIN_DECIBEL_POWER_THRESHOLD = 0.3
     }
 }
